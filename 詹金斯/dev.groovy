2e5ac36c4919e2639c4afc4e2c0c578aa80901ee
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2019-07-07 12:57:50 +0800

    Update and rename jenkins/dev.groovy to 詹金斯/dev.groovy

---
+++ 詹金斯/dev.groovy
@@ -0,0 +1,13 @@
+def name="";
+
+node {  
+    stage('check') { 
+        echo("check")
+    }
+    stage('install') { 
+        echo("install")
+    }
+    stage('build') { 
+        echo("build")
+    }
+}
