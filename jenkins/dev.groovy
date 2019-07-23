9e4c0b1782d8667e1a7059ed43fe30607d9814c8
zhangzhiwei321 <1041943804@qq.com>
2019-07-23 14:44:21 +0800

    脚本

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -3,12 +3,40 @@ def name="";
 node {  
     stage('check') { 
         echo("check");
-		echo(install);
+		echo(name);
+		try{
+			git branch
+			git checkout  master
+			git pull 
+		}catch(e){
+			git clone  git@github.com:zhangzhiwei321/jenkins-test1.git
+			git branch
+			git checkout  master
+			git pull 
+		}
+		
     }
     stage('install') { 
         echo("install")
+		echo(install);
+		if(install){
+			rm -rf node_modules*
+			cnpm i
+		}else{
+			
+		}
     }
     stage('build') { 
         echo("build")
+		npm run build
     }
+	stage('build') { 
+	    echo("build")
+		npm run build
+	}
+	stage("release"){
+		echo("release")
+		rm -f /usr/local/nginx/web/vue/*
+		mv dist/* /usr/local/nginx/web/vue
+	}
 }
