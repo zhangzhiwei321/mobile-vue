c8905fb565d26c3a4bece13a2bffab2ca9626ac7
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2019-07-27 21:02:49 +0800

    Update dev.groovy

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -11,7 +11,7 @@ node {
 		sh	"git pull" 
 		}catch(e){
 		sh	"git clone  https://github.com/zhangzhiwei321/jenkins-test1.git"
-		sh      "cd jenkins-test1"
+		sh      "cd /var/lib/jenkins/workspace/gtihub1/jenkins-test1"
 		sh	"git branch"
 		sh	"git checkout  master"
 		sh	"git pull" 
