5ac81b4c3b280615c7db60d69483b94241f37680
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2019-07-28 00:28:23 +0800

    Update dev.groovy

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -5,7 +5,7 @@ node {
         echo("check");
 	echo(name);
 	    try{
-		sh      "find /var/lib/jenkins/workspace/gtihub1/jenkins-test1 -maxdepth 1"
+		sh      "git status"
 	    }catch(e){
 		sh	"git clone  git@github.com:zhangzhiwei321/jenkins-test1.git"
 
