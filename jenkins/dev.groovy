703a1befc1e706b5b1f286b5d8ab0d06b20f7b1b
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2019-07-27 23:23:08 +0800

    Update dev.groovy

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -5,10 +5,10 @@ node {
         echo("check");
 	echo(name);
 	    try{
-		sh      "cd /var/lib/jenkins/workspace/gtihub1/jenkins-test1"   
+		sh	"git pull https://github.com/zhangzhiwei321/jenkins-test1.git" 
 	    }catch(e){
 		sh	"git clone  https://github.com/zhangzhiwei321/jenkins-test1.git"
-		sh      "cd /var/lib/jenkins/workspace/gtihub1/jenkins-test1
+
 	    }
 	    	sh	"git status"
 		sh	"git pull https://github.com/zhangzhiwei321/jenkins-test1.git" 
