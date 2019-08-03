49b82089bd8b5797f7e93404c4d0846717ede2b1
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2019-08-04 00:23:45 +0800

    Update dev.groovy

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -2,8 +2,8 @@ def name="";
 
 node {  
     stage('check') { 
-        echo("check");
-	echo(name);	
+        	echo("check");
+		echo(name);	
 	         "rm -rf /var/lib/jenkins/workspace/Pipeline-test1/jenkins-test1/*"
 	    	 'git clone git@github.com:zhangzhiwei321/jenkins-test1.git/'
 		 'git status'
@@ -15,20 +15,18 @@ node {
         echo("install")
 		echo(install);
 		if(install){
-		sh	"rm -rf node_modules*"
-		sh	"cnpm i"
-		}else{
-			
+		"rm -rf node_modules*"
+		"cnpm i"
 		}
     }
     stage('build') { 
-        echo("build")
-		sh     "cnpm run build"
+        	echo("build")
+		"npm run build"
     }
 
 	stage("release"){
 		echo("release")
-	sh	"rm -rf /usr/local/nginx/web/vue/*"
-	sh	"mv dist/* /usr/local/nginx/web/vue"
+		"rm -rf /usr/local/nginx/web/vue/*"
+		"mv dist/* /usr/local/nginx/web/vue"
 	}
 }
