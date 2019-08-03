26567814e4aa4a5bc49296e72c391f9b0ff9b6e9
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2019-08-04 00:40:55 +0800

    Update dev.groovy

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -4,29 +4,28 @@ node {
     stage('check') { 
         	echo("check");
 		echo(name);	
-	         "rm -rf /var/lib/jenkins/workspace/Pipeline-test1/jenkins-test1/*"
-	    	 sh 'git clone git@github.com:zhangzhiwei321/jenkins-test1.git/'
-		 'git status'
-	         'git pull origin dev'
-	    	 'git branch'
-	    	 'git checkout dev'
+
+		 sh 'git status'
+	         sh 'git pull origin dev'
+	    	 sh 'git branch'
+	    	 sh 'git checkout dev'
     }
     stage('install') { 
         echo("install")
 		echo(install);
 		if(install){
-		"rm -rf node_modules*"
-		"cnpm i"
+		sh "rm -rf node_modules*"
+		sh "cnpm i"
 		}
     }
     stage('build') { 
         	echo("build")
-		"npm run build"
+		sh "npm run build"
     }
 
 	stage("release"){
 		echo("release")
-		"rm -rf /usr/local/nginx/web/vue/*"
-		"mv dist/* /usr/local/nginx/web/vue"
+		sh "rm -rf /usr/local/nginx/web/vue/*"
+		sh"mv dist/* /usr/local/nginx/web/vue"
 	}
 }
