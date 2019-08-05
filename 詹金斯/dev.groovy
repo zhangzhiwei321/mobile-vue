8837c9bf2735a884b2428238f1e32018347db1d1
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2019-08-05 19:13:14 +0800

    Update and rename jenkins/dev.groovy to 詹金斯/dev.groovy

--- 詹金斯/dev.groovy
+++ 詹金斯/dev.groovy
@@ -5,22 +5,11 @@ node {
     stage('check') { 
         	echo("check");
 		echo(name);
-		if [ -d "./Pipeline-script/.git" ];then
-		echo "文件夹存在"
 	    	sh 'cd jenkins-test1'
 	        git 'pull origin dev'
 	    	git 'status'
 	    	git 'branch'
 	    	git 'checkout dev'
-		else
-		echo "文件夹不存在"
-		git clone https://github.com/zhangzhiwei321/jenkins-test1.git
-	    	sh 'cd jenkins-test1'
-	        git 'pull origin dev'
-	    	git 'status'
-	    	git 'branch'
-	    	git 'checkout dev'
-		fi
     }
     stage('install') { 
         echo("install")
