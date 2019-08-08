34be4b34a76053e705ee748b6c57b7d90ab85657
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2019-08-08 14:31:01 +0800

    Update dev.groovy

--- jenkins/dev.groovy
+++ jenkins/dev.groovy
@@ -1,32 +1,34 @@
 #!/bin/bash -ilex
 def name="";
 
-node {  
-    stage('check') { 
-        	echo("check");
-		echo(name);
-	    	sh 'cd jenkins-test1'
-	        sh 'git pull origin dev'
-	    	sh  'git status'
-	    	sh   'git branch'
-	    	sh  'git checkout dev'
+node { 
+ stage('check') { 
+ echo("check");
+ echo(name);
+ sh 'ls'
+ sh 'cd jenkins-test1&&git pull origin dev'
+ sh 'ls'
+ sh 'cd jenkins-test1&&git pull origin dev'
+ sh 'cd jenkins-test1&&git status'
+ sh 'cd jenkins-test1&&git branch'
+ sh 'cd jenkins-test1&&git checkout dev'
     }
-    stage('install') { 
-        echo("install")
-		echo(install);
-		if(install){
-		sh "rm -rf node_modules*"
-		sh "cnpm i"
+ stage('install') { 
+ echo("install")
+ echo(install);
+ if(install){
+ sh "cd jenkins-test1&&rm -rf node_modules*"
+ sh "cd jenkins-test1&&cnpm i"
 		}
     }
-    stage('build') { 
-        	echo("build")
-		sh "npm run build"
+ stage('build') { 
+ echo("build")
+ sh "cd jenkins-test1&&npm run build"
     }
 
-	stage("release"){
-		echo("release")
-		sh "rm -rf /usr/local/nginx/web/vue/*"
-		sh"mv dist/* /usr/local/nginx/web/vue"
+ stage("release"){
+ echo("release")
+ sh "rm -rf /web/vue/*"
+ sh"mv ./jenkins-test1/dist/* /web/vue"
 	}
 }
