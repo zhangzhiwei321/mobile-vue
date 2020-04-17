51187a222c2848b684b07f1113cd12ed1ca0e9d5
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2020-04-18 00:56:46 +0800

    Update test.groovy

--- jenkins/test.groovy
+++ jenkins/test.groovy
@@ -18,7 +18,7 @@ pipeline {
         }
         stage('Deploy') { 
             steps {
-                sh "npm install --registry https://registry.npm.taobao.org"
+                sh "npm install --registry=https://registry.npm.taobao.org"
             }
         }
     }
