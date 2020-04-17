d8b9a1b5787b0bb00eff3fd5226747c841a7af91
zhangzhiwei321 <33408997+zhangzhiwei321@users.noreply.github.com>
2020-04-18 01:46:34 +0800

    Update test.groovy

--- jenkins/test.groovy
+++ jenkins/test.groovy
@@ -9,9 +9,6 @@ pipeline {
             steps {
                 sh "echo install"
             }
-            steps {
-                sh "npm run build"
-            }
         }
         stage('Test') { 
             agent any
